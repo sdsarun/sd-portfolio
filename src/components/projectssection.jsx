68d7b1c06cb7be00a2e3a245bf687f4d9f33dddf
Sarun Daunghirun <125266560+sdsarun@users.noreply.github.com>
2023-06-25 03:01:29 +0700

    Add demo link at kostkan project

--- src/components/projectssection.jsx
+++ src/components/projectssection.jsx
@@ -14,7 +14,7 @@ export default function ProjectsSection({ image, title, description, date, tools
             </header>
             <section className="flex flex-col gap-y-8 px-6 py-6">
                 <Project title="JustFit" description="Activity Tracking." date={DateUtil.getDateFormat()} image={justfitIcon} videoBackground={justfitDemoVideo} tools={["ReactJS", "ExpressJS", "NodeJS", "MongoDB"]} githubLink={"https://github.com/jaturaorn/Activity-tracker-app"} />
-                <Project title="Kostkan" description="Postman clone minimal." date={DateUtil.getDateFormat()} image={justfitIcon} videoBackground={kostkanVideo} tools={["ReactJS"]} githubLink={"https://github.com/sdsarun/kostkan"} />
+                <Project title="Kostkan" description="Postman clone minimal." date={DateUtil.getDateFormat()} image={justfitIcon} videoBackground={kostkanVideo} tools={["ReactJS"]} githubLink={"https://github.com/sdsarun/kostkan"} demoLink={"https://kostkan.vercel.app/"} />
                 <Project title="Kotes" description="Minimal notes taking." date={DateUtil.getDateFormat()} image={justfitIcon} videoBackground={kotesVideo} tools={["HTML", "CSS", "JavaScript"]} githubLink={"https://github.com/sdsarun/portfolio/tree/main/kotes"} />
             </section>
         </section>
