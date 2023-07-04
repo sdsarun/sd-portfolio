d2f53f3dc7792559e9b17c10fd38b384630126ad
Sarun Daunghirun <125266560+sdsarun@users.noreply.github.com>
2023-07-04 15:21:50 +0700

    Add just-fit demo link

--- src/components/projectssection.jsx
+++ src/components/projectssection.jsx
@@ -13,7 +13,7 @@ export default function ProjectsSection({ image, title, description, date, tools
                 <h1 className="text-white text-4xl font-bold py-10 pr-8 text-right">{"< PROJECTS />"}</h1>
             </header>
             <section className="flex flex-col gap-y-8 px-6 py-6">
-                <Project title="JustFit" description="Activity Tracking." date={DateUtil.getDateFormat()} image={justfitIcon} videoBackground={justfitDemoVideo} tools={["ReactJS", "ExpressJS", "NodeJS", "MongoDB"]} githubLink={"https://github.com/jaturaorn/Activity-tracker-app"} />
+                <Project title="JustFit" description="Activity Tracking." date={DateUtil.getDateFormat()} image={justfitIcon} videoBackground={justfitDemoVideo} tools={["ReactJS", "ExpressJS", "NodeJS", "MongoDB"]} githubLink={"https://github.com/jaturaorn/Activity-tracker-app"} demoLink={"https://just-fit-sdsarun.vercel.app/"} />
                 <Project title="Kostkan" description="Postman clone minimal." date={DateUtil.getDateFormat()} image={justfitIcon} videoBackground={kostkanVideo} tools={["ReactJS"]} githubLink={"https://github.com/sdsarun/kostkan"} demoLink={"https://kostkan.vercel.app/"} />
                 <Project title="Kotes" description="Minimal notes taking." date={DateUtil.getDateFormat()} image={justfitIcon} videoBackground={kotesVideo} tools={["HTML", "CSS", "JavaScript"]} githubLink={"https://github.com/sdsarun/portfolio/tree/main/kotes"} />
             </section>
